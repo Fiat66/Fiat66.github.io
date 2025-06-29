fd62ab19c2fb3c605d9eb3baeae6bb22d33e8bc2
anuphap witsukumarn <anuphap.w@en.rmutt.ac.th>
2025-06-29 16:22:07 +0700

    create index.html file

---
+++ todo-app/app.js
@@ -0,0 +1,22 @@
+
+
+addTodo = () => {
+    const todoText = todoInput.ariaValueMax.trim();
+    if (todoText.length > 0){
+        const todoObject = {
+            Text: todoText,
+            completed: false
+        }
+        all
+    }
+}
+
+
+createTodoItem = () => {
+
+
+}
+updateTodoList = () => {
+
+
+}
